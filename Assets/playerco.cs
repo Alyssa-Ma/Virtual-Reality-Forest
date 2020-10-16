d4ee0d280c245721871cc1b81271efdc560dadad
unknown <victorhuangh411@yahoo.com>
2020-10-16 19:01:09 -0400

    google cardboard pointer

---
+++ Assets/playerco.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class playerco : MonoBehaviour
+{
+   void OnCollisionEnter (Collision collisionInfo){
+	if (collisionInfo.collider.name == "Friend 3-Megan"){
+	
+		Debug.Log(collisionInfo.collider.name);
+		}	
+	}
+
+}
