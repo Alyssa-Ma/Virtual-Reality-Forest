aee271157638adf7595ab7b0cb4b2f9bee3880d0
unknown <victorhuangh411@yahoo.com>
2020-11-03 02:19:49 -0500

    FlashLight,Battery,GAZE

---
+++ Assets/Battery.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Battery : MonoBehaviour 
+{
+	public float batteryPower;
+	public void OnTriggerEnter(Collider other)
+	{
+		if(other.CompareTag("GameController"))
+		{
+		GameObject.FindGameObjectWithTag("Flash_Light").GetComponent<Flash_Light>().AddBatteryLife(batteryPower);
+		Destroy(gameObject);
+		}
+	}
+	
+	
+}
--- Assets/Flash_Light.cs
+++ Assets/Flash_Light.cs
@@ -1,78 +1,56 @@
 ﻿using UnityEngine;
 using System.Collections;
- 
-public class Flash_Light : MonoBehaviour {
- 
-    public float a;
-    public float b;
-    public float c;
-    public float d;
- 
-    public int batLevel;
- 
-    public Light FLight;
-    public bool isOn;
-    public float timer;
- 
-    void Start()
+
+public class Flash_Light : MonoBehaviour 
+{
+	public KeyCode flashlightToggleKey = KeyCode.F;	
+	public AudioClip toggleSound;
+	public float batteryLifeSeconds = 60f;
+	public float bat_life = 0;
+	
+	
+	public float maxIntensity= 10f;
+	
+	public float batteryLife;
+	
+	private bool isActive;
+	
+	private AudioSource myAudioSource;
+	private Light myLight;
+	
+	void Start()
+	{
+		myLight = GetComponent<Light>();
+		batteryLife = myLight.intensity;
+		
+	}
+	
+	void Update()
+	{
+		
+		
+		myLight.enabled = true;
+		myLight.intensity -= batteryLife / batteryLifeSeconds * Time.deltaTime;
+		bat_life = myLight.intensity*10;
+		
+		
+		
+		
+	}
+	
+	public void AddBatteryLife(float _batteryPower)
+	{
+		myLight.intensity += _batteryPower;
+		if(myLight.intensity > maxIntensity);
+			myLight.intensity = maxIntensity;
+	}
+
+	
+	  void OnGUI()
     {
-        FLight = GetComponent<Light>();
-        batLevel = 100;
-        minusBat();
-        isOn = true;
+        GUI.Box(new Rect(0, Screen.height / 1.21f, Screen.width / 6.16f, Screen.height / 19.58f),bat_life.ToString());
     }
- 
-    void minusBat()
-    {
-        if (isOn)
-        {
-            batLevel -= 10;
-        }
-    }
- 
-    void Update()
-    {
- 
-        if(timer >= 0)
-        {
-            if (isOn)
-            {
-                timer -= Time.deltaTime;
-            }
-        }
- 
-        if (timer <= 0)
-        {
-            timer = 2;
-            minusBat();
-        }
- 
-        if (Input.GetKeyUp(KeyCode.F))
-        {
-            FLight.enabled = !FLight.enabled;
- 
-            if (!isOn)
-            {
-                isOn = true;
-            }
-            else
-            {
-                isOn = false;
-            }
- 
-        }
- 
-        if(batLevel == 0)
-        {
-            batLevel = 0;
-            FLight.enabled = false;
-            isOn = false;
-        }
- 
-    }
- 
-    void OnGUI()
-    {
-        GUI.Box(new Rect(0, Screen.height / 1.21f, Screen.width / 6.16f, Screen.height / 19.58f), batLevel.ToString());
-    }
+
+
+
 }
