40c542ed0248f7873dc34844135475781b4d2330
Michelle Lucero <31500626+mlucero4607@users.noreply.github.com>
2020-11-16 21:21:58 -0500

    added gaze interaction for log activity

---
+++ Assets/Dropping.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dropping : MonoBehaviour
+{
+    public Rigidbody holdable;
+    //dropping position
+    public GameObject dropPos;
+    public GameObject pedestal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        holdable.transform.parent = null;
+        holdable.transform.localPosition = dropPos.transform.localPosition;
+        holdable.transform.rotation = Quaternion.identity;
+        holdable.transform.rotation = Quaternion.Euler(0, 0, 90);
+        dropPos.SetActive(false);
+        pedestal.layer = 0;
+    }
+}
