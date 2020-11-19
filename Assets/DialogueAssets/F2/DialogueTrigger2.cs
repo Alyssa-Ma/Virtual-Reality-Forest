0602ebc16f1bae7709d0528adeec5ca3308f71d7
Alyssa <Alyssa.Ma80@myhunter.cuny.edu>
2020-11-19 06:05:24 -0500

    complete dialogue

---
+++ Assets/DialogueAssets/F2/DialogueTrigger2.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger2 : MonoBehaviour
+{
+    public Dialogue2 dialogue;
+
+    public void TriggerDialogue ()
+    {
+        FindObjectOfType<DialogueManager2>().StartDialogue(dialogue);
+    }
+}
