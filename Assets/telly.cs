bc6f7deaa2816bb646f0fbdbef50b8c1527aeb4e
008705110 <roflmaoness300@yahoo.com>
2020-11-20 00:14:10 -0500

    test3

---
+++ Assets/telly.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class telly : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        col.transform.position = new Vector3 (12, 11, -21);
+    }
+
+}
